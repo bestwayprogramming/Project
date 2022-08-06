916733e802aa6676efe0164ea942fc0a99566a8e
Vivek <apple@Vivek-s-MacBook-Pro.local>
2022-08-06 08:06:58 +0530

    Main Project

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function list(){
+    document.getElementsByClassName('hover-show')[0].style.display="inline-block";
+    var click=document.getElementsByClassName('head2')[0].style;
+    click.backgroundColor="rgb(0, 0, 0,1)";
+    click.borderRadius="10px";
+}
